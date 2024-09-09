a975bcead442dd4beb9dfd85692e3aa7fb7946eb
IsaiasRB123 <isaiwenhill@gmail>
2024-09-08 19:50:15 -0500

    mejora 1

--- src/App.js
+++ src/App.js
@@ -56,8 +56,7 @@ function App() {
     <BrowserRouter>
       <MyContext.Provider value={values}>
 
-        {/* Renderiza el contenido que necesita usar `useLocation` */}
-        <AppContent />
+        
 
         <Header />
         <div className='main d-flex'>
@@ -90,42 +89,6 @@ function App() {
   );
 }
 
-function AppContent() {
-  const location = useLocation(); // Hook dentro de un componente envuelto por BrowserRouter
-  const [isToggleSidebar, setIsToggleSidebar] = useState(false); // Estado del Sidebar
-
-  return (
-    <>
-      {location.pathname !== '/login' && <Header />}
-      <div className='main d-flex'>
-        {location.pathname !== '/login' && (
-          <div className={`sidebarWrapper ${isToggleSidebar === true ? 'toggle' : ''}`}>
-            <Sidebar />
-          </div>
-        )}
-        <div className={`content ${isToggleSidebar === true ? 'toggle' : ''}`}>
-          <Routes>
-            <Route path="/" exact={true} element={<Dashboard />} />
-            <Route path="/dashboard" exact={true} element={<Dashboard />} />
-            <Route path="/categories" exact={true} element={<Categories />} />
-            <Route path="/appointment" exact={true} element={<Appointment />} />
-            <Route path="/orders" exact={true} element={<Orders />} />
-            <Route path="/products" exact={true} element={<Products />} />
-            <Route path="/programming" exact={true} element={<Programming />} />
-            <Route path="/sales" exact={true} element={<Sales />} />
-            <Route path="/services" exact={true} element={<Services />} />
-            <Route path="/shopping" exact={true} element={<Shopping />} />
-            <Route path="/suppliers" exact={true} element={<Suppliers />} />
-            <Route path="/users" exact={true} element={<Users />} />
-            <Route path="/roles" exact={true} element={<Roles />} />
-            <Route path="/login" exact={true} element={<Login />} />
-            <Route path="/absences" exact={true} element={<Absences />} />
-          </Routes>
-        </div>
-      </div>
-    </>
-  );
-}
 
 export default App;
 export { MyContext };
