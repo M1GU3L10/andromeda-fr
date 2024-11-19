cd2463aa0d97e1909e9f2cb403eb3a3ea02a7b12
IsaiasRB123 <isaiwenhill@gmail>
2024-11-18 19:02:22 -0500

    arre-2

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -637,12 +637,12 @@ finally {
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="contained" onClick={validar} className='btn-success'>
-              Guardar
-            </Button>
-            <Button variant="outlined" onClick={handleClose} className='btn-secondary'>
-              Cerrar
-            </Button>
+          <Button variant="primary" onClick={validar} className='btn-sucess'>
+                            Guardar
+                        </Button>
+                        <Button variant="secondary" onClick={handleClose} id='btnCerrar' className='btn-red'>
+                            Cerrar
+                        </Button>
           </Modal.Footer>
         </Modal>
          <Modal show={showDetailModal} onHide={handleCloseDetail}>
