02fa2b3fab05c26bc396351dceba00636397ddbb
joeltd15 <joeltuiran15@gmail.com>
2024-12-04 16:36:04 -0500

    añañai

--- src/pages/index/Header/index.jsx
+++ src/pages/index/Header/index.jsx
@@ -130,7 +130,7 @@ const Header = ({ scrollToServices, scrollToContact }) => {
                                 {userEmail}
                             </Button>
                             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} className='menu-landingPage'>
-                                {userRole == 1 || userRole == 38 ? (
+                                {userRole == 1 || userRole == 2 ? (
                                     <MenuItem onClick={handledashboard} className='menu-item-landingPage'>
                                         <GrUserAdmin /> Administrar
                                     </MenuItem>
